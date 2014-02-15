5e88835ec4650ede00e52c8a1fdb35706ad0386e
Ng Wee Kiat Jeremy <jeremy.ng.wk1990@gmail.com>
2014-02-16 01:12:40 +0800

    correct wget code for pubmeds for camp_seq series

--- src/bash/wget_pubmeds_camp_seq.sh
+++ src/bash/wget_pubmeds_camp_seq.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 mkdir ~/Desktop/lsm3241-misc/data/camp_SEQ_data_html/pubmeds
-cp ~/Desktop/lsm3241-misc/data/camp_SEQ_data_html/camp_seq_ref_id.txt ~/Desktop/lsm3241-misc/data/camp_SEQ_data_html/pubmeds
+cp ~/Desktop/lsm3241-misc/data/camp_SEQ_data_html/camp_seq_ref.txt ~/Desktop/lsm3241-misc/data/camp_SEQ_data_html/pubmeds
 
 cd ~/Desktop/lsm3241-misc/data/camp_SEQ_data_html/pubmeds
 
-awk 'print{wget -O" $1 $1"}'camp_seq_ref_id.txt| while read line;
+awk '{print "wget -O " $1 ".html " $2}' camp_seq_ref.txt>temp.txt
+sed '/wget -O .html/d' temp.txt> wgetcall.txt
+
+while read line;
 do
 `$line`;
 sleep 0.5;
-done # performs the wget
+done < wgetcall.txt # performs the wget
