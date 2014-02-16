28989a470b7b0e2412d8cd1e1f51859a79b0284a
Ng Wee Kiat Jeremy <jeremy.ng.wk1990@gmail.com>
2014-02-16 23:43:08 +0800

    remove R codes for parsing, migrate to bash instead

--- src/R/parse_activity_CAMP.R
+++
@@ -1,7 +0,0 @@
-# R CMD TO parse temp.txt
-
-init <- readLines("~/Desktop/lsm3241-misc/data/camp_SEQ_data_html/temp.txt") # reads in temp.txt from the initially parsed file
-final <- init[grep("Anti",init)]
-
-write.table(final,"~/Desktop/lsm3241-misc/data/camp_SEQ_data_html/temp.txt")
-
