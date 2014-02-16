4376e6295cfca9a7603093b8d5539c4f085e967a
Ng Wee Kiat Jeremy <jeremy.ng.wk1990@gmail.com>
2014-02-16 23:59:16 +0800

    added comments and tidying up codes in bash script; added TO DO:

--- src/bash/parse_html.sh
+++ src/bash/parse_html.sh
@@ -1,48 +1,61 @@
 #!/bin/bash
 
-# install dos2unix using homebrew
-#brew install dos2unix
+# series of bash scripts to parse html files for building flat file DB for query
+# brew install dos2unix # install dos2unix using homebrew
+
 
 export LC_CTYPE=C
 export LANG=C
 
+############################################################################
+# TO DO:
+# parse LAMP data for activity to add to the DB flatfile
+############################################################################
 
-
-# series of bash scripts to parse html files for building flat file DB for query
-
+########################################################################################################################################################
 ## to parse CAMP_SEQ series
-#
-# to get the species of origins
+
 for i in $(seq 1 5047);
 do
    dos2unix CAMP_SEQ$i.html;
 done # converts all the files from DOS to unix
 
-## to get species
+mkdir curated-CAMP # makes a directory where the processed data will be stored
+
+# Chunk 1: Species of origin of the AMP
+#Species of origin for the CAMP seq are found in camp_seq_species.txt in the curated data folder
 grep -A 1 "Source" *.html|grep "left"| sed -e 's/^[ \t]*//g' -e 's/<td align="left">//g' -e 's|</td>||g' > temp.txt # temp file for storing the data
 cat temp.txt| tr -s ' '|sed -e 's/.html//g' -e  's/-/,/g' -e 's/\[[^]]*\]//g'> ./curated-CAMP/camp_seq_species.txt
-rm temp.txt # clears the temp file, the species of origin for the CAMP seq are found in camp_seq_species.txt
+rm temp.txt # clears the temp file
+# End C1 #
 
-## to get sequence
+# Chunk 2: To get AMP sequence
+# CAMP sequences are found in camp_seq_squence.txt
 grep "fasta" *html|grep "justify"| sed -e  's/<td colspan="2" align="justify" class="fasta">//' -e 's|<br></td>||g'>temp.txt #temp file only
 cat temp.txt| tr -s ' '|sed -e 's/.html//g' -e  's/:/,/g' -e 's/<br>//g'> ./curated-CAMP/camp_seq_sequence.txt
-rm temp.txt # CAMP sequences are found in camp_seq_squence.txt
+rm temp.txt
+# End C2 #
 
-## to get length
+## Chunk 3: To get AMP length
+# Length stored in camp_seq_length.txt
 grep -A 1 "Length" *.html|grep "left"| sed -e 's/^[ \t]*//g' -e 's/<td align="left">//g' -e 's|</td>||g' > temp.txt # temp file for storing the data
 cat temp.txt| tr -s ' '|sed -e 's/.html//g' -e  's/-/,/g' -e 's/\[[^]]*\]//g'> ./curated-CAMP/camp_seq_length.txt
 rm temp.txt
+# End C3 #
 
-## to get the NCBI references::
-grep -A 1 "pubmed" *html| sed -e 's/^[ \t]*//g' -e 's/<td align="left"><a href=//g' -e 's/<[^>]*>//g'>temp.txt
-cat temp.txt| tr -s ' '|sed -e 's/, [ 0-9 ]* target=_blank>.*//g'| sed -e 's/CAMP_SEQ[0-9]*.html-//g' -e 's/--//g'  -e 's/,[ 0-9 ]*//g' -e 's/.html:/ /g' -e 's/target=_blank.*//g' >./curated-CAMP/camp_seq_ref.txt # references file; to be added to the flatfile
+# C4: To get AMP activity
+grep -A 1 "Activity" *.html| sed -e 's/<[^>]*>//g' -e 's/^[\t]*//g'| grep "Anti" >temp.txt # temp.txt needs to be parsed in R::
+awk '{print $1 }' temp.txt| sed 's/"//g' > ./curated-CAMP/activityindex.txt
+awk '{print $2 $3 $4}' temp.txt| sed 's/"//g' > ./curated-CAMP/activity.txt
+# End C4 #
 
-rm temp.txt  # removes our temp file
+## to generate separate list for easy manipulation in R ##
+# Scripts for parsing prior to submission into R scripts #
 
-## to generate separate list for easy manipulation in R
-# read into R with readLines
-# list of index for sequence and species
-cd ./curated-CAMP/ # goes to the directory where all the flat files are stored..
+cd ./curated-CAMP/ # goes to the directory where all the flat files are stored
+
+# each file will be split into 2 files- one for the index (CAMP ID) and the other for the corresponding values (Seq, Length, Species)
+# Separating will make working in R easier, each file will be read in via readLines()
 cut -d "," -f2- camp_seq_species.txt > camp_seq_species_id.txt
 cut -d "," -f1 camp_seq_species.txt > camp_seq_species_index.txt
 cut -d "," -f2- camp_seq_sequence.txt > camp_seq_sequence_id.txt
@@ -50,19 +63,19 @@ cut -d "," -f1 camp_seq_sequence.txt > camp_seq_sequence_index.txt
 cut -d "," -f2- camp_seq_length.txt > camp_seq_length_id.txt
 cut -d "," -f1 camp_seq_length.txt > camp_seq_length_index.txt
 
-# to get activity
-grep -A 1 "Activity" *.html| sed -e 's/<[^>]*>//g' -e 's/^[\t]*//g'| grep "Anti" >temp.txt # temp.txt needs to be parsed in R::
-awk '{print $1 }' temp.txt| sed 's/"//g' > ./curated-CAMP/activityindex.txt
-awk '{print $2 $3 $4}' temp.txt| sed 's/"//g' > ./curated-CAMP/activity.txt
+########################################################################################################################################################
 
+########################################################################################################################################################
 # to parse LAMP series of data
 mkdir curated-LAMP
 grep "Source" *.html|sed -e 's/^.*Source://g' -e 's/<.*//g'>./curated-LAMP/species.txt # parses out for the species of each lamp name
 ls *html|sed -e 's/.html//g'>./curated-LAMP/index.txt # list of all the IDs:
+
 grep "Length" *.html|sed -e 's/^.*Length://g' -e 's/<.*//g'>./curated-LAMP/sequencelength.txt # gets the length of aa sequence
 grep "Length" *.html|sed -e 's/^.*Sequence<br>//g' -e 's/<.*//g' -e 's/[&nbsp;]*//g'>./curated-LAMP/sequences.txt # gets the aa sequence
 # there is a file with a funny new line- we will parse it with R in the R codes
-# to run with R
+
+# to run with R to make the flatfile
 R CMD BATCH ../R/construct_flat_seq.R # R stout output will be stored in construct_flat_seq.Rout in the source directory
 
 
